7dd95f55f0eae46f944e481a0b981334167592df
VivesPascalVanWijk <pascal.vanwijk@student.vives.be>
2026-05-18 14:26:28 +0200

    Add controller

---
+++ WarehouseManager.Ui.Mvc/Controllers/StockManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarehouseManager.Ui.Mvc.Controllers
+{
+    public class StockManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
