3cdce34c3b1cccb1b4e0e509930ae3c45252fb49
VivesPascalVanWijk <pascal.vanwijk@student.vives.be>
2026-05-18 14:32:43 +0200

    Add repository

---
+++ WarehouseManager.Repository/WarehouseManagerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseManager.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WarehouseManager.Repository
+{
+    public class WarehouseManagerDbContext : DbContext
+
+    {
+        public DbSet<Article> Articles => Set<Article>();
+
+        public DbSet<Warehouse> Warehouses => Set<Warehouse>();
+
+        public void MockData()
+        {
+
+        }
+    }
+}
