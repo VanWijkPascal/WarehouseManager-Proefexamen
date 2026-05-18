26793cc4f9b664e37006c90deea4957723761d79
VivesPascalVanWijk <pascal.vanwijk@student.vives.be>
2026-05-18 16:15:52 +0200

    Register services

--- WarehouseManager.Ui.Mvc/Program.cs
+++ WarehouseManager.Ui.Mvc/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WarehouseManager.Repository;
+using WarehouseManager.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,23 +17,26 @@ builder.Services.AddDbContext<WarehouseManagerDbContext>(options =>
 });
 
 // Register Services
-
-
-
-
-
-
-
-
+builder.Services.AddScoped<ArticleService>();
+builder.Services.AddScoped<WarehouseService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<WarehouseManagerDbContext>();
+    //if (dbContext.Database.IsInMemory())
+    //{
+    dbContext.FillInMockData();
 }
 
 app.UseHttpsRedirection();
